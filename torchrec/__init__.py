461434cc009f8a94589b24520d71d4da29b94404
zuoxin.xiahou <xiahouzuoxin@163.com>
2024-07-17 23:22:19 +0800

    add imports in init

--- torchrec/__init__.py
+++ torchrec/__init__.py
@@ -1,1 +1,8 @@
+from .dataset import DataFrameDataset, FeatureTransformer
+from .trainer import Trainer
+from .sample import traintest_split, traintest_split_by_date
+from .utils import auto_generate_feature_configs 
+
+from .embedding import DynamicEmbedding
+
 __version__ = "0.1.0"
